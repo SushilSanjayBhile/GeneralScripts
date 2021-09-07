dd902e0304272cc31c9abe70648f6e86fb7bb3a4
sushil <sushil@diamanti.com>
2021-09-07 05:37:51 -0700

    changed solutionc copy script

--- copyfiles/solutions_copy_files.sh
+++ copyfiles/solutions_copy_files.sh
@@ -3,6 +3,7 @@ set -x
 
 # clean rpm directory
 ssh diamanti@vagserv1 -t  "rm ~/rpms/*"
+ssh diamanti@vagserv1 -t  "mkdir ~/rpms"
 
 # copy rpm files from current directory to vagserv1
 scp ./*.rpm diamanti@vagserv1:~/rpms/
@@ -16,5 +17,12 @@ ssh diamanti@vagserv1 -t "scp ~/rpms/*.rpm solcntl:~/rpms/"
 # clean controllers rpms directory
 ssh diamanti@vagserv1 -t "ssh diamanti@solcntl -t \"ssh diamanti@solserv10 -t 'rm ~/rpms/*'\""
 
+# create rpms directory
+ssh diamanti@vagserv1 -t "ssh solcntl -t \"ssh solserv12 -t \"mkdir ~/rpms\"\""
+ssh diamanti@vagserv1 -t "ssh solcntl -t \"ssh solserv11 -t 'mkdir ~/rpms/'\""
+ssh diamanti@vagserv1 -t "ssh solcntl -t \"ssh solserv10 -t 'mkdir ~/rpms/'\""
+
 # copy rpm files from vagserv to solcntl
+ssh diamanti@vagserv1 -t "ssh solcntl -t \"scp ~/rpms/* solserv12:~/rpms/\""
 ssh diamanti@vagserv1 -t "ssh solcntl -t \"scp ~/rpms/* solserv10:~/rpms/\""
+ssh diamanti@vagserv1 -t "ssh solcntl -t \"scp ~/rpms/* solserv11:~/rpms/\""
