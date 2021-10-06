c3fc086b9470658f137606de735c1a00e338fcfc
sushil <sushil@diamanti.com>
2021-10-06 01:31:25 -0700

    updated script to copy rpms

--- copyfiles/solutions_copy_files.sh
+++ copyfiles/solutions_copy_files.sh
@@ -2,20 +2,13 @@
 set -x
 
 # clean rpm directory
-ssh diamanti@vagserv1 -t  "rm ~/rpms/*"
 ssh diamanti@vagserv1 -t  "mkdir ~/rpms"
 
 # copy rpm files from current directory to vagserv1
 scp ./*.rpm diamanti@vagserv1:~/rpms/
 
-# clean controllers rpms directory
-ssh diamanti@vagserv1 -t "ssh diamanti@solcntl -t \"rm rpms/*\""
-
 # copy rpm files from vagserv to solcntl
 ssh diamanti@vagserv1 -t "scp ~/rpms/*.rpm solcntl:~/rpms/"
-
-# clean controllers rpms directory
-ssh diamanti@vagserv1 -t "ssh diamanti@solcntl -t \"ssh diamanti@solserv10 -t 'rm ~/rpms/*'\""
 
 # create rpms directory
 ssh diamanti@vagserv1 -t "ssh solcntl -t \"ssh solserv12 -t \"mkdir ~/rpms\"\""
